e51d671abfa468916b603fbf59cdfe31c96c6a9c
benjaminmo <51932735+Bennet187@users.noreply.github.com>
2020-02-21 16:54:23 +0100

    Update bootstrap.php

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -3,7 +3,7 @@
 use jtl\Connector\Presta\Mapper\PrimaryKeyMapper;
 
 require '../../config/config.inc.php';
-$loader = require 'vendor/autoload.php';
+$loader = require 'library/autoload.php';
 
 const TEST_DIR = __DIR__;
 
